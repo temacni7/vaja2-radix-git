875619b35ac2a87c826dd2241cfaa00bba307bfb
temacni7 <mark.markez@student.um.si>
2026-03-21 16:05:30 +0100

    Implementirano branje in validacija vhodnih števil

--- main.cpp
+++ main.cpp
@@ -4,12 +4,35 @@
 
 using namespace std;
 
-bool Branje_Stevil(vector<unsigned char>& vec, const char filename[]);
+bool Branje_Stevil(vector<unsigned char>& vec, const char filename[]) {
+    ifstream input(filename);
+    int st;
+
+    if (!input.is_open()) {
+        return false;
+    }
+
+    while (input >> st) {
+        if (st < 0 || st > 255) {
+            input.close();
+            return false;
+        }
+
+        vec.push_back(static_cast<unsigned char>(st));
+    }
+
+    input.close();
+    return true;
+}
 
 int main(int argc, const char* argv[]) {
     vector<unsigned char> A;
 
     if (argc != 2) {
+        return 0;
+    }
+
+    if (!Branje_Stevil(A, argv[1])) {
         return 0;
     }
 
