586fa6cc92ef09a39a32b04a4fab9fc69a61a638
temacni7 <mark.markez@student.um.si>
2026-03-21 15:44:40 +0100

    Dodana osnova za branje vhodnih podatkov

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool Branje_Stevil(vector<unsigned char>& vec, const char filename[]);
 
 int main(int argc, const char* argv[]) {
+    vector<unsigned char> A;
+
+    if (argc != 2) {
+        return 0;
+    }
+
     return 0;
 }
